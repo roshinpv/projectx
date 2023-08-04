6f6d2d285e7132962c9a6eb5302eef52d0484fe5
roshinpv <roshinpv@gmail.com>
2023-08-04 20:55:35 +0530

    Initial version of quickgrab generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/wf/grab/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.wf.grab.domain;
---
+++ src/test/java/com/wf/grab/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.wf.grab.cucumber;
+
+import com.wf.grab.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
